d70e7b742b78036e94b928e6052a1ffbb336a3d6
Marta Lokit <lokit2002@mail.ru>
2023-05-15 00:49:39 +0300

    Data take from browser

---
+++ Cashe/Cashe/Controllers/CasheController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace Cashe.Controllers
+{
+    public class CasheController : Controller
+    {
+        private const string KeyName = "CasheKeyName";
+        private readonly IMemoryCache _memory;
+        public CasheController(IMemoryCache memory)
+        {
+            _memory = memory;
+        }
+        [HttpGet("[action]")]
+        public IActionResult SetCache(string? data, int second=10)
+        {
+            data ??= DateTime.Now.ToString("F");//формат времени   
+            _memory.Set(KeyName, data, DateTime.Now.AddSeconds(second));
+            return Ok($"Data in cache for {second}");
+        }
+        [HttpGet("[action]")]
+        public IActionResult GetCache()
+        {
+            var isExsists= !_memory.TryGetValue(KeyName, out string? value);
+            var message = isExsists
+                ? "No data in cache"
+                : $"Data in cache {value}";
+            return Ok(message);
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
