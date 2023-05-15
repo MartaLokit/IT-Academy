1cfcd5fdd621c7e505c268596c172a48fd2019be
Marta Lokit <lokit2002@mail.ru>
2023-05-15 19:49:34 +0300

    Library

---
+++ Book/Book/DataBaseContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Book.Models;
+namespace Book
+{
+    public class DataBaseContext:DbContext
+    {
+        public DbSet<Books> book { get; set; }
+        public DbSet<Author> author { get; set; }
+        public DataBaseContext(DbContextOptions<DataBaseContext> options):base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Books>().ToTable("Book");
+            modelBuilder.Entity<Author>().ToTable("Author");
+        }
+    }
+}
---
+++ Book/Book/Models/Author.cs
@@ -0,0 +1,20 @@
+﻿namespace Book.Models
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public Author(string firstName, string lastname, string surName)
+        {
+            LastName = lastname;
+            FirstName = firstName;
+            Surname = surName;
+        }
+        public Author()
+        {
+
+        }
+    }
+}
