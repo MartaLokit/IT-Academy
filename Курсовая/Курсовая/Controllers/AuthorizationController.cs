d66a7cc76e4a7e9ff8d9b15454d04b29950e009a
Marta Lokit <lokit2002@mail.ru>
2023-08-04 11:50:57 +0300

    Commit 1

---
+++ Курсовая/Курсовая/Controllers/AuthorizationController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Курсовая.Controllers
+{
+    public class AuthorizationController : Controller
+    {
+        private readonly DataBaseContext _baseContext;
+        public AuthorizationController(DataBaseContext baseContext)
+        {
+            _baseContext = baseContext;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var user = _baseContext.DataUsers.Include(c => c.ID);
+            return View("~/Views/Authorization/Index.cshtml", await _baseContext.DataUsers.ToListAsync());
+        }
+    }
+}
